c66aad9515db9a730b79a7e67050bff752110d02
gonikaagrawal-arch <gonikaagarwal@gmail.com>
2025-09-11 01:29:46 +0530

    Add files via upload

---
+++ que1assignment2.c
@@ -0,0 +1,16 @@
+//wap a c program to calculate the area and perimeter of the reactangle based on its length and width
+#include<stdio.h>
+int main(){
+    int length , width, area, perimeter;
+    printf("enter the length of the reactangle:");
+    scanf("%d", &length);
+    printf("enter the width of the reactangle:");
+    scanf("%d", &width);
+    area = length*width;
+    perimeter = 2*(length +width);
+    printf("area of the reactangle is :%d/n", area);
+    printf("perimeter of the reactangle is :%d/n",perimeter);
+    return 0;
+}
+    
+
