84d84da38abbccfeef8a145c2716968e0a82bcf2
gonikaagrawal-arch <gonikaagarwal@gmail.com>
2025-09-11 07:54:32 +0530

    Add files via upload

---
+++ que3assignment2.c
@@ -0,0 +1,14 @@
+//wap a c program to add numbers and print the sum of the two numbers
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    printf("Enter the first number:");
+    scanf("%d",&a);
+    printf("Enter the second number:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("The sum of %d and %d is:%d\n",a,b,sum);
+    return 0;
+}
+    
+    
