95043391e8075ebe08a1c25fb806a36b4c19aa5c
gonikaagrawal-arch <gonikaagarwal@gmail.com>
2025-09-11 16:29:45 +0530

    Add files via upload

---
+++ que3assignment3.c
@@ -0,0 +1,7 @@
+//write a c program that takes an integer input and uses the bitwise AND(&)operator along with ternary operator along with a ternary operator to check whether the number is even or odd
+#include<stdio.h>
+int main(){
+    
+
+
+}
