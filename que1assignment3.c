08918d6bac26acba13d32d26ca2becbb977e0b18
gonikaagrawal-arch <gonikaagarwal@gmail.com>
2025-09-11 16:28:59 +0530

    Add files via upload

---
+++ que1assignment3.c
@@ -0,0 +1,11 @@
+// write a program to print the size of various data types in c 
+
+#include<stdio.h>
+int main(){
+    printf("size of int: %d \n", sizeof(int));
+    printf("size of float:%d\n", sizeof(float));
+    printf("size of char:%d\n", sizeof(char));
+    printf("size of double:%d\n", sizeof(double));
+    
+    return 0;
+}
